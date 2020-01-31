6fbf20100dc81c7f1ef66e26068923ad45a291f4
Samuel Ullman <samullman@gmail.com>
2020-01-31 10:38:13 -0800

    update csp

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,6 +28,7 @@ module.exports = {
           "default-src": "'self'", 
           "script-src": "'self' 'unsafe-eval'", 
           "connect-src": "'self'", 
+          "worker-src": "'self' 'unsafe-eval'",
           "font-src": "'self' data: fonts.gstatic.com",
         }
       }
