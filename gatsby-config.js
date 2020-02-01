dca77fff6c420681ef2d079d814cf6cdaf178882
Samuel Ullman <samullman@gmail.com>
2020-01-31 21:33:49 -0800

    update config

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,7 +29,7 @@ module.exports = {
           "script-src": "'self' 'unsafe-eval'", 
           "connect-src": "'self'", 
           "worker-src": "blob:",
-          "script-src-elem": "blob:"
+          "script-src-elem": "blob:",
           "font-src": "'self' data: fonts.gstatic.com",
         }
       }
